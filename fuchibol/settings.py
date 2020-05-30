81aa80abfa17018a04c568ed9c40d616abc52e05
Angel Barrios <abarriosgiron2@gmail.com>
2020-05-30 11:51:55 -0600

    update setting 2.0

--- fuchibol/settings.py
+++ fuchibol/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://tanqueapp.herokuapp.com']
+ALLOWED_HOSTS = ['tanqueapp.herokuapp.com']
 
 
 # Application definition
