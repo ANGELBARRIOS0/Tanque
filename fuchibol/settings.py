e2d7f3c00edec089b34de613e613e9ced7e4ead4
ANGELBARRIOS0 <44789497+ANGELBARRIOS0@users.noreply.github.com>
2020-07-09 18:30:47 -0600

    Update settings.py

--- fuchibol/settings.py
+++ fuchibol/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'byjy@i&cvev)95w6+#d3bpu76(b0^8x(p%def1n)zu6&t)c1d&'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'byjy@i&cvev)95w6+#d3bpu76(b0^8x(p%def1n)zu6&t)c1d&'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
